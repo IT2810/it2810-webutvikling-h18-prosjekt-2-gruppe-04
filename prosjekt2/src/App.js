a352eb7d7ace5ab9881d263ee801528900aee3a6
Lars Syversen <larsksy@stud.ntnu.no>
2018-09-07 15:44:42 +0200

    #5 setup general layout of application

--- prosjekt2/src/App.js
+++ prosjekt2/src/App.js
@@ -5,14 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="grid-wrapper">
+          <div className="header">
+          </div>
+          <div className="topbar">
+          </div>
+          <div className="sidebar">
+          </div>
+          <div className="gallery">
+          </div>
       </div>
     );
   }
