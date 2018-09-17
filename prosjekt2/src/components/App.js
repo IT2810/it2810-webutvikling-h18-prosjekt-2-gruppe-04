e5dfd9b1fd6b835955efceb611f91b07f8f100d5
Andreas Ore Larssen <andreasol@me.com>
2018-09-17 12:04:47 +0200

    Add rules for mobile menu toggle and resize-listeners.

--- prosjekt2/src/components/App.js
+++ prosjekt2/src/components/App.js
@@ -8,11 +8,38 @@ import '../styles/App.css';
 import Gallery from "./Gallery";
 
 class App extends Component {
+    componentDidMount() {
+        window.addEventListener("resize", this.resize.bind(this));
+        this.resize();
+    }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.resize.bind(this));
+    }
+    resize() {
+
+        if (window.innerWidth > 780) {
+            this.setState({ isMenuHidden: false });
+        }
+        else{
+            this.setState({ isMenuHidden: true });
+        }
+    }
+
+    constructor(props){
+        super(props);
+        this.menuToggle = this.menuToggle.bind(this);
+        this.state = {isMenuHidden:false}
+    }
+    menuToggle(){
+        this.setState(state => ({ isMenuHidden: !state.isMenuHidden }));
+    }
+
+
   render() {
     return (
       <div className="grid-wrapper">
-          <Header/>
-          <Topbar/>
+          <Header onMenuToggle={this.menuToggle} isMenuHidden={this.state.isMenuHidden}/>
+          <Topbar isMenuHidden={this.state.isMenuHidden}/>
           <Sidebar/>
           <Gallery/>
       </div>
--- prosjekt2/src/components/Header.js
+++ prosjekt2/src/components/Header.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import '../styles/Header.css';
-
 /*Det ble avklart at vi kan bruke Font Awesome i prosjektet for å legge inn ikoner, etc.
  Link for oppklaring:https://piazza.com/class/jl25sg0g4ql5nk?cid=39*/
 import {library} from '@fortawesome/fontawesome-svg-core';
@@ -12,12 +11,18 @@ library.add(faBars, faTimes);
 
 class Header extends Component {
 
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
-            <div className="header">
+            <div className="header" style={{borderBottom: (this.props.isMenuHidden) ? "4px solid #3700fe" : "4px solid white"}}>
                 <h1>My awesome gallery</h1>
-                <div className="burger"><a href="#"><FontAwesomeIcon icon="bars"/></a></div>
-                <div className="cross"><a href="#"><FontAwesomeIcon icon="times"/></a></div>
+                <div className="menuButton" onClick={this.props.onMenuToggle}>{ this.props.isMenuHidden ? (
+                    <FontAwesomeIcon icon="bars"/>
+                ) : (
+                    <FontAwesomeIcon icon="times"/>
+                )}</div>
             </div>
         );
     }
